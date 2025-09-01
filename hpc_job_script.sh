2ff5f3d26c7ffcc208c0d07d4de06aef628b0651
cppprp <angelika.svetlove@gmail.com>
2025-09-01 13:26:57 +0200

    increasing limit time

--- hpc_job_script.sh
+++ hpc_job_script.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=14
 #SBATCH --gpus=1
 #SBATCH --mem-per-gpu=128789
-#SBATCH --time=04:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=logs/plankton_%j.out
 #SBATCH --error=logs/plankton_%j.err
 
